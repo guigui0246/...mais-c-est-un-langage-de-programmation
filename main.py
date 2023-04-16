1df2535cd0c8941e8dc88a58d5673f882e0b8122
unknown <guigui0246@yahoo.com>
2023-04-16 04:47:38 +0200

    [ADD] add main

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+
+if __name__ != "__main__":
+    def printf(*args, **kwargs) -> None:
+        print(*args, **kwargs, file=sys.stderr)
+else:
+    def printf(*args, **kwargs) -> None:
+        print(*args, **kwargs)
+
+from entier import entier
+from flottant import flottant
+
+i = entier(3)
+j = flottant(6)
+k = i + j
+k = entier(k)
+j = flottant(i)
+j += k
+k = flottant(k)
+j += k
+
+printf(j)
