93d148970c504b2825e544e6ead411a1ddae926e
unknown <guigui0246@yahoo.com>
2023-04-16 04:58:23 +0200

    [ADD] add other type de la même manière que le précédant commit

--- types.py
+++ types.py
@@ -3,6 +3,14 @@ class types(object):
         return entier(*args, **kwargs)
     def flottant(self, *args, **kwargs):
         return flottant(*args, **kwargs)
+    def texte(self, *args, **kwargs):
+        return texte(*args, **kwargs)
+    def liste(self, *args, **kwargs):
+        return liste(*args, **kwargs)
+    def liste_const(self, *args, **kwargs):
+        return liste_const(*args, **kwargs)
+    def dico(self, *args, **kwargs):
+        return dico(*args, **kwargs)
 
 class entier(int):
     def entier(self, *args, **kwargs):
@@ -11,3 +19,19 @@ class entier(int):
 class flottant(float):
     def flottant(self, *args, **kwargs):
         return flottant(*args, **kwargs)
+
+class texte(str):
+    def texte(self, *args, **kwargs):
+        return texte(*args, **kwargs)
+
+class liste(list):
+    def liste(self, *args, **kwargs):
+        return liste(*args, **kwargs)
+
+class liste_const(tuple):
+    def liste_const(self, *args, **kwargs):
+        return liste_const(*args, **kwargs)
+
+class dico(dict):
+    def dico(self, *args, **kwargs):
+        return dico(*args, **kwargs)
