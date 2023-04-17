32c9b401fbfcc388427723ff6b60a2c2987df72d
guigui0246 <guillaume.deplaine@epitech.eu>
2023-04-17 11:16:00 +0200

    [ADD] add new file (:... no needed)

--- mot_clef.py
+++ mot_clef.py
@@ -12,56 +12,63 @@ class replace():
 
 liste_replace = []
 
-liste_replace.append(replace("str", "texte"))
-liste_replace.append(replace("int", "entier"))
-liste_replace.append(replace("float", "flottant"))
-liste_replace.append(replace("complex", "complèxe"))
-liste_replace.append(replace("list", "liste"))
-liste_replace.append(replace("tuple", "liste_const"))
-liste_replace.append(replace("range", "liste_continue"))
-liste_replace.append(replace("dict", "dico"))
-liste_replace.append(replace("set", "groupe"))
-liste_replace.append(replace("frozenset", "groupe_const"))
-liste_replace.append(replace("bool", "booléen"))
-liste_replace.append(replace("bytes", "octets"))
-liste_replace.append(replace("bytearray", "liste_octet"))
-liste_replace.append(replace("memoryview", "vue_mémoire"))
+try:
+    from types import add_types
+    liste_replace = add_types(liste_replace)
+except ModuleNotFoundError:
+    import sys
+    print("Types non chargés", file=sys.stderr)
+except ImportError:
+    pass
 
-liste_replace.append(replace("None", "Vide"))
-liste_replace.append(replace("False", "Faux"))
-liste_replace.append(replace("True", "Vrai"))
-liste_replace.append(replace("and", "et"))
-liste_replace.append(replace("as", "comme"))
-liste_replace.append(replace("assert", "vérifier"))
-liste_replace.append(replace("break", "casser_boucle"))
-liste_replace.append(replace("class", "classe"))
-liste_replace.append(replace("continue", "prochaine_itération"))
-liste_replace.append(replace("def", "définir"))
-liste_replace.append(replace("del", "supprimer"))
-liste_replace.append(replace("elif", "et_si"))
-liste_replace.append(replace("else", "sinon"))
-liste_replace.append(replace("except", "rattraper"))
-liste_replace.append(replace("finally", "finalement"))
-liste_replace.append(replace("for", "pour"))
-liste_replace.append(replace("from", "depuis"))
-liste_replace.append(replace("global", "globale"))
-liste_replace.append(replace("if", "si"))
-liste_replace.append(replace("import", "importer"))
-liste_replace.append(replace("in", "dans"))
-liste_replace.append(replace("is", "est"))
-liste_replace.append(replace("lambda", "lambda"))
-liste_replace.append(replace("nonlocal", "non_locale"))
-liste_replace.append(replace("not", "non"))
-liste_replace.append(replace("or", "ou"))
-liste_replace.append(replace("pass", "passer"))
-liste_replace.append(replace("raise", "remonter"))
-liste_replace.append(replace("return", "renvoyer"))
-liste_replace.append(replace("try", "essayer"))
-liste_replace.append(replace("while", "tant_que"))
-liste_replace.append(replace("with", "avec"))
-liste_replace.append(replace("yield", "renvoyer_générateur"))
+try:
+    from error import add_errors
+    liste_replace = add_errors(liste_replace)
+except ModuleNotFoundError:
+    import sys
+    print("Erreurs non chargées", file=sys.stderr)
+except ImportError:
+    pass
 
+def add_keywords(liste_replace:list[replace]) -> list[replace]:
+    liste_replace.append(replace("None", "Vide"))
+    liste_replace.append(replace("False", "Faux"))
+    liste_replace.append(replace("True", "Vrai"))
+    liste_replace.append(replace("and", "et"))
+    liste_replace.append(replace("as", "comme"))
+    liste_replace.append(replace("assert", "vérifier"))
+    liste_replace.append(replace("async", "asynchroniser"))
+    liste_replace.append(replace("await", "attendre"))
+    liste_replace.append(replace("break", "casser_boucle"))
+    liste_replace.append(replace("class", "classe"))
+    liste_replace.append(replace("continue", "prochaine_itération"))
+    liste_replace.append(replace("def", "définir"))
+    liste_replace.append(replace("del", "supprimer"))
+    liste_replace.append(replace("elif", "et_si"))
+    liste_replace.append(replace("else", "sinon"))
+    liste_replace.append(replace("except", "rattraper"))
+    liste_replace.append(replace("finally", "finalement"))
+    liste_replace.append(replace("for", "pour"))
+    liste_replace.append(replace("from", "depuis"))
+    liste_replace.append(replace("global", "globale"))
+    liste_replace.append(replace("if", "si"))
+    liste_replace.append(replace("import", "importer"))
+    liste_replace.append(replace("in", "dans"))
+    liste_replace.append(replace("is", "est"))
+    liste_replace.append(replace("lambda", "lambda"))
+    liste_replace.append(replace("nonlocal", "non_locale"))
+    liste_replace.append(replace("not", "non"))
+    liste_replace.append(replace("or", "ou"))
+    liste_replace.append(replace("pass", "passer"))
+    liste_replace.append(replace("raise", "remonter"))
+    liste_replace.append(replace("return", "renvoyer"))
+    liste_replace.append(replace("try", "essayer"))
+    liste_replace.append(replace("while", "tant_que"))
+    liste_replace.append(replace("with", "avec"))
+    liste_replace.append(replace("yield", "renvoyer_générateur"))
+    return liste_replace
 
+liste_replace = add_keywords(liste_replace)
 
 def remplacer(string:str) -> str | None:
     global liste_replace
--- types_objet.py
+++ types_objet.py
@@ -1,70 +1,95 @@
-class types(object):
-    def entier(self, *args, **kwargs):
-        return entier(*args, **kwargs)
-    def flottant(self, *args, **kwargs):
-        return flottant(*args, **kwargs)
-    def texte(self, *args, **kwargs):
-        return texte(*args, **kwargs)
-    def liste(self, *args, **kwargs):
-        return liste(*args, **kwargs)
-    def liste_const(self, *args, **kwargs):
-        return liste_const(*args, **kwargs)
-    def dico(self, *args, **kwargs):
-        return dico(*args, **kwargs)
-    def complèxe(self, *args, **kwargs):
-        return complèxe(self, *args, **kwargs)
-    def types(self, *args, **kwargs):
-        return types(self, *args, **kwargs)
-    def groupe(self, *args, **kwargs):
-        return groupe(*args, **kwargs)
-    def groupe_const(self, *args, **kwargs):
-        return groupe_const(*args, **kwargs)
-    def octets(self, *args, **kwargs):
-        return octets(self, *args, **kwargs)
-    def liste_octets(self, *args, **kwargs):
-        return liste_octets(*args, **kwargs)
+##class types(object):
+##    def entier(self, *args, **kwargs):
+##        return entier(*args, **kwargs)
+##    def flottant(self, *args, **kwargs):
+##        return flottant(*args, **kwargs)
+##    def texte(self, *args, **kwargs):
+##        return texte(*args, **kwargs)
+##    def liste(self, *args, **kwargs):
+##        return liste(*args, **kwargs)
+##    def liste_const(self, *args, **kwargs):
+##        return liste_const(*args, **kwargs)
+##    def dico(self, *args, **kwargs):
+##        return dico(*args, **kwargs)
+##    def complèxe(self, *args, **kwargs):
+##        return complèxe(self, *args, **kwargs)
+##    def types(self, *args, **kwargs):
+##        return types(self, *args, **kwargs)
+##    def groupe(self, *args, **kwargs):
+##        return groupe(*args, **kwargs)
+##    def groupe_const(self, *args, **kwargs):
+##        return groupe_const(*args, **kwargs)
+##    def octets(self, *args, **kwargs):
+##        return octets(self, *args, **kwargs)
+##    def liste_octets(self, *args, **kwargs):
+##        return liste_octets(*args, **kwargs)
+##
+##
+##class entier(int):
+##    def entier(self, *args, **kwargs):
+##        return entier(*args, **kwargs)
+##
+##class flottant(float):
+##    def flottant(self, *args, **kwargs):
+##        return flottant(*args, **kwargs)
+##
+##class texte(str):
+##    def texte(self, *args, **kwargs):
+##        return texte(*args, **kwargs)
+##
+##class liste(list):
+##    def liste(self, *args, **kwargs):
+##        return liste(*args, **kwargs)
+##
+##class liste_const(tuple):
+##    def liste_const(self, *args, **kwargs):
+##        return liste_const(*args, **kwargs)
+##
+##class dico(dict):
+##    def dico(self, *args, **kwargs):
+##        return dico(*args, **kwargs)
+##
+##class complèxe(complex):
+##    def complèxe(self, *args, **kwargs):
+##        return complèxe(self, *args, **kwargs)
+##
+##class groupe(set):
+##    def groupe(self, *args, **kwargs):
+##        return groupe(*args, **kwargs)
+##
+##class groupe_const(frozenset):
+##    def groupe_const(self, *args, **kwargs):
+##        return groupe_const(*args, **kwargs)
+##
+##class octets(bytes):
+##    def octets(self, *args, **kwargs):
+##        return octets(self, *args, **kwargs)
+##
+##class liste_octets(bytearray):
+##    def liste_octets(self, *args, **kwargs):
+##        return liste_octets(*args, **kwargs)
 
 
-class entier(int):
-    def entier(self, *args, **kwargs):
-        return entier(*args, **kwargs)
-
-class flottant(float):
-    def flottant(self, *args, **kwargs):
-        return flottant(*args, **kwargs)
-
-class texte(str):
-    def texte(self, *args, **kwargs):
-        return texte(*args, **kwargs)
-
-class liste(list):
-    def liste(self, *args, **kwargs):
-        return liste(*args, **kwargs)
-
-class liste_const(tuple):
-    def liste_const(self, *args, **kwargs):
-        return liste_const(*args, **kwargs)
-
-class dico(dict):
-    def dico(self, *args, **kwargs):
-        return dico(*args, **kwargs)
-
-class complèxe(complex):
-    def complèxe(self, *args, **kwargs):
-        return complèxe(self, *args, **kwargs)
-
-class groupe(set):
-    def groupe(self, *args, **kwargs):
-        return groupe(*args, **kwargs)
-
-class groupe_const(frozenset):
-    def groupe_const(self, *args, **kwargs):
-        return groupe_const(*args, **kwargs)
-
-class octets(bytes):
-    def octets(self, *args, **kwargs):
-        return octets(self, *args, **kwargs)
-
-class liste_octets(bytearray):
-    def liste_octets(self, *args, **kwargs):
-        return liste_octets(*args, **kwargs)
+try:
+    from keyword import replace
+    def add_types(liste_replace:list[replace]) -> list[replace]:
+        liste_replace.append(replace("str", "texte"))
+        liste_replace.append(replace("int", "entier"))
+        liste_replace.append(replace("float", "flottant"))
+        liste_replace.append(replace("complex", "complèxe"))
+        liste_replace.append(replace("list", "liste"))
+        liste_replace.append(replace("tuple", "liste_const"))
+        liste_replace.append(replace("range", "liste_continue"))
+        liste_replace.append(replace("dict", "dico"))
+        liste_replace.append(replace("set", "groupe"))
+        liste_replace.append(replace("frozenset", "groupe_const"))
+        liste_replace.append(replace("bool", "booléen"))
+        liste_replace.append(replace("bytes", "octets"))
+        liste_replace.append(replace("bytearray", "liste_octet"))
+        liste_replace.append(replace("memoryview", "vue_mémoire"))
+        return liste_replace
+except ModuleNotFoundError:
+    def add_types(liste_replace:list) -> list:
+        import sys
+        print("Types non chargés", file=sys.stderr)
+        return liste_replace
